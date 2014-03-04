aa27c2575a5259840d0724261ecb72626159ec42
hogh <kasperhhogh@gmail.com>
2014-03-04 20:41:09 +0100

    Testgraf under statistik

--- v2/js/mainv2.js
+++ v2/js/mainv2.js
@@ -3,29 +3,39 @@
  */
 $(document).ready(
 function() {
+    hidePages()
+    $("body").find("#home-page").show();
+    $("#home-link").on("click", function() {
         hidePages();
         $("body").find("#home-page").show();
-        $("#home-link").on("click", function() {
-            hidePages();
-            $("body").find("#home-page").show();
-        });
-        $("#friends-link").on("click", function() {
-            hidePages();
-            $("body").find("#friends-page").show();
-        });
-        $("#statistik-link").on("click", function() {
-            hidePages()
-            $("body").find("#statistik-page").show();
-        });
-        $("#settings-link").on("click", function() {
-            hidePages()
-            $("body").find("#settings-page").show();
-        });
     });
+    $("#friends-link").on("click", function() {
+        hidePages();
+        $("body").find("#friends-page").show();
+    });
+    $("#statistik-link").on("click", function() {
+        hidePages();
+        $("body").find("#statistik-page").show();
+        drawGraph();
+    });
+    $("#settings-link").on("click", function() {
+        hidePages();
+        $("body").find("#settings-page").show();
+    });
+});
 
 function hidePages() {
     $("#home-page").hide();
     $("#statistik-page").hide();
     $("#friends-page").hide();
     $("#settings-page").hide();
+}
+
+function drawGraph() {
+    var line1=[['2008-09-30 4:00PM',4], ['2008-10-30 4:00PM',6.5], ['2008-11-30 4:00PM',5.7], ['2008-12-30 4:00PM',9], ['2009-01-30 4:00PM',8.2]];
+    var plot1 = $.jqplot('chart1', [line1], {
+        title:'Default Date Axis',
+        axes:{xaxis:{renderer:$.jqplot.DateAxisRenderer}},
+        series:[{lineWidth:4, markerOptions:{style:'square'}}]
+    });
 }
