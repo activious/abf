445aee7b64d608c77afddcd136de8c9faecd3f2e
hogh <kasperhhogh@gmail.com>
2014-03-13 00:42:44 +0100

    Skrå labels

--- v2/js/mainv2.js
+++ v2/js/mainv2.js
@@ -138,8 +138,10 @@ function drawGraph() {
     var line1=[['2014-02-25 00:00AM',40], ['2014-02-26 00:00AM',33], ['2014-02-27 00:00AM',37], ['2014-02-28 00:00AM',41], ['2014-03-01 00:00AM',39], ['2014-03-02 00:00AM',29], ['2014-03-03 00:00AM',47], ['2014-03-04 00:00AM',40]];
     plot = $.jqplot('chart1', [line1], {
         title:'Consumption of electricity last 7 days',
-        axes:{xaxis:{renderer:$.jqplot.DateAxisRenderer,
-            tickOptions:{formatString:'%m/%#d-%y'},
+        axes:{xaxis:{
+            renderer:$.jqplot.DateAxisRenderer,
+            tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+            tickOptions:{formatString:'%m/%#d-%y', angle: -30},
             min:'2014-02-25',
             tickInterval:'1 day'
         },
